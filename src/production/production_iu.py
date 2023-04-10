6cb04a73168795265a74843cb59e4d96b97c31fe
mhanauer <mhanauer@indiana.edu>
2023-04-10 18:47:00 -0400

    Removed packages

--- src/production/production_iu.py
+++ src/production/production_iu.py
@@ -1,5 +1,3 @@
-import xgboost as xgb
-
 import pandas as pd
 import os
 from pyprojroot import here
@@ -21,20 +19,14 @@ st.title('Prediction app for IU making the NCAA tournament')
 
 w = st.number_input('Enter wins:', min_value=0.0, step=0.01, value = 15.0)
 l = st.number_input('Enter losses:', min_value=0.0, step=0.01, value = 15.0)
-w_l_percent = st.number_input('Enter w_l_%:', min_value=0.0, step=0.01, value = .5)
 w_c = st.number_input('Enter conference wins:', min_value=0.0, step=0.01, value = 10.0)
 l_c = st.number_input('Enter conference losses:', min_value=0.0, step=0.01, value = 10.0)
-w_l_percent_c = st.number_input('Enter w_l_%_c:', min_value=0.0, step=0.01, value = .5)
-srs = st.number_input('Enter srs:', min_value=0.0, step=0.01, value = 20.0)
 sos = st.number_input('Enter strength of schedule (sos):', min_value=0.0, step=0.01, value = 20.0)
 ps_g = st.number_input('Enter points per game:', min_value=0.0, step=0.01, value = 75.0)
 pa_g = st.number_input('Enter points allowed per game:', min_value=0.0, step=0.01, value = 70.0)
 ap_pre = st.number_input('Enter AP preseason ranking:', min_value=0.0, step=0.01, value = 25.0)
-ap_high = st.number_input('Enter ap_high:', min_value=0.0, step=0.01, value = 20.0)
-ap_final = st.number_input('Enter ap_final:', min_value=0.0, step=0.01, value = 20.0)
-seed = st.number_input('Enter seed:', min_value=0, step=1, value = 11)
-
-input_data = np.array([w, l, w_l_percent, w_c, l_c, w_l_percent_c, srs, sos, ps_g, pa_g, ap_pre, ap_high, ap_final, seed]).reshape(1, -1)
+ap_final = st.number_input('Enter AP high ranking:', min_value=0.0, step=0.01, value = 25.0)
+input_data = np.array([w, l, w_c, l_c, sos, ps_g, pa_g, ap_pre, ap_final]).reshape(1, -1)
 
 if st.button('Predict'):
     prediction = model_iu_bball.predict(input_data)
