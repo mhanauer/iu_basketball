329036148f449f7538024ce4608184e259764038
mhanauer <mhanauer@indiana.edu>
2023-04-12 15:13:15 -0400

    Added percentage function

--- src/production/production_iu.py
+++ src/production/production_iu.py
@@ -17,19 +17,19 @@ import joblib
 
 st.title('Prediction app for IU making the NCAA tournament')
 
-w = st.number_input('Enter wins:', min_value=0.0, step=0.01, value = 15.0)
-l = st.number_input('Enter losses:', min_value=0.0, step=0.01, value = 15.0)
-w_c = st.number_input('Enter conference wins:', min_value=0.0, step=0.01, value = 10.0)
-l_c = st.number_input('Enter conference losses:', min_value=0.0, step=0.01, value = 10.0)
-sos = st.number_input('Enter strength of schedule (sos):', min_value=0.0, step=0.01, value = 20.0)
+w = st.number_input('Enter wins:', min_value=0.0, step=0.01, value = 22.0)
+l = st.number_input('Enter losses:', min_value=0.0, step=0.01, value = 12.0)
+w_c = st.number_input('Enter conference wins:', min_value=0.0, step=0.01, value = 11.0)
+l_c = st.number_input('Enter conference losses:', min_value=0.0, step=0.01, value = 8.0)
+sos = st.number_input('Enter strength of schedule (sos):', min_value=0.0, step=0.01, value = 10.0)
 ps_g = st.number_input('Enter points per game:', min_value=0.0, step=0.01, value = 75.0)
-pa_g = st.number_input('Enter points allowed per game:', min_value=0.0, step=0.01, value = 70.0)
-ap_pre = st.number_input('Enter AP preseason ranking:', min_value=0.0, step=0.01, value = 25.0)
-ap_final = st.number_input('Enter AP high ranking:', min_value=0.0, step=0.01, value = 25.0)
+pa_g = st.number_input('Enter points allowed per game:', min_value=0.0, step=0.01, value = 67.0)
+ap_pre = st.number_input('Enter AP preseason ranking:', min_value=0.0, step=0.01, value = 15.0)
+ap_final = st.number_input('Enter AP high ranking:', min_value=0.0, step=0.01, value = 22.0)
 input_data = np.array([w, l, w_c, l_c, sos, ps_g, pa_g, ap_pre, ap_final]).reshape(1, -1)
 
 if st.button('Predict'):
     prediction = model_iu_bball.predict(input_data)
-    rounded_prediction = round(prediction[0], 2)
-    st.write(f'The prediction is: {rounded_prediction}')
+    percentage_prediction = round(prediction[0] * 100, 0)
+    st.write(f'The prediction is: {percentage_prediction}%')
 
